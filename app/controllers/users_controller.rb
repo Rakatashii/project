0358236a8c86bdb333359fe695f5d574753053d8
Christian Meyer <cmeyerwps@gmail.com>
2018-03-04 21:40:11 -0500

    users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def create
+    debugger
+    @user = User.new(params[:user])    # Not the final implementation!
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
 end
