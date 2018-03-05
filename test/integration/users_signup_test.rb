3a77fed146c0d5c4f5840a7cf7f1bb7456886ece
Christian Meyer <cmeyerwps@gmail.com>
2018-03-04 23:01:49 -0500

    working signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: 
+                                   { name:  "",
+                                   email: "user@invalid",
+                                   password: "foo",
+                                   password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    #assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div#error_explanation'
+    #assert_select 'div.<CSS class for field with error'
+    assert_select 'div.field_with_errors'
+  end
+=begin
+  test "invalid signup information" do 
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: "",
+                                         email: "user@invalid",
+                                         password: "foo",
+                                         password_confimation: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert.alert-danger'
+    assert_select "li", "Name can't be blank"
+    assert_select "li", "Email is invalid"
+    assert_select "li", "Password confirmation doesn't match Password"
+    min_validation = User.validators_on(:password).find do |v|
+      v.options.key?(:minimum)
+    end
+    min_length = min_validation.options[:minimum]
+    assert_select "li", "Password is too short (minimum is 5 characters)"
+  end
+=end
+end
