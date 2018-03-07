13e8746a5942e0d65c59a2dc26870902e8aa5d41
Christian Meyer <cmeyerwps@gmail.com>
2018-03-06 20:44:54 -0500

    routes_login_invalidation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+  def create
+    user = User.find_by(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password]
+      # Log user in
+    else
+      # Error
+      render 'new' # Rerender w/ invalidation info
+    end
+  end
+  def destroy
+  end
 end
