961366bc6afa9df422d524de4c0ca81bf774fdd1
Christian Meyer <cmeyerwps@gmail.com>
2018-03-08 10:32:44 -0500

    helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  # "Application helper should be avoided, as it usually just becomes a place to dump unused code later."
   def full_title(page_title = '')
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
