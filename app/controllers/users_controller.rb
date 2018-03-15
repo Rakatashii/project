d4d1a5318b193a526c964fae94a3e0749e24ec17
Christian Meyer <cmeyerwps@gmail.com>
2018-03-15 19:36:57 -0400

    admins+destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
-  
+  before_action :admin_user,     only: [:destroy]
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
   def show # GET /users/:id
     @user = User.find(params[:id])
@@ -35,6 +35,12 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  def destroy
+    User.find(params[:id]).destroy
+    #Note: cannot do @user.destroy, why? @user has not yet been defined. Cannot rely on @user automatically being set by #correct_user. Again, why? b/c #before_action :correct_user does not include :destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
 
   private
 
@@ -54,5 +60,8 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id]) # This is how @user is defined for the #edit/#create methods
       redirect_to(root_url) unless current_user?(@user)
+    end
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
