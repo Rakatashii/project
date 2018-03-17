f80bf229cbf62e4930d6dd4d7fd7064da51c59b8
Christian Meyer <cmeyerwps@gmail.com>
2018-03-16 20:21:12 -0400

    kinda fucked up rn

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,8 +26,12 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+# AFTER rails generate mailer...
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' } # WATCH - can change to https later
+# end
 
   config.action_mailer.perform_caching = false
 
