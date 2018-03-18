bcda000b196e8cb6b8cc3f68817d02f271f7d98f
Christian Meyer <cmeyerwps@gmail.com>
2018-03-18 19:19:42 -0400

    domain name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => ''587',
+    :port           => '587',
     :protocol       => 'https' # WATCH
     :authentication => 'plain',
     :user_name      => ENV['SENDGRID_USERNAME'],
