6744ceaac2c7d427861a5fe106e1370cbcaa4979
Christian Meyer <cmeyerwps@gmail.com>
2018-03-21 19:44:45 -0400

    password controller - before_actions

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -14,6 +14,7 @@ class UserMailer < ApplicationMailer
   #   en.user_mailer.password_reset.subject
   
   def password_reset(user)
+    # https://glacial-chamber-68532.herokuapp.com//password_resets/MBe6ubKeHVYpJ_IfPehcIQ/edit?email=cmeyerwps%40gmail.com
     @user = user
     mail to: user.email, subject: "Password Reset"
   end
