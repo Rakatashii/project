27572f63ec82e4610d78b7544adb17dc15d16b96
Christian Meyer <cmeyerwps@gmail.com>
2018-03-22 13:07:57 -0400

    password meths

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,7 @@
 class PasswordResetsController < ApplicationController
   before_action :get_user,   only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
   
   def new
   end
@@ -16,18 +17,37 @@ class PasswordResetsController < ApplicationController
       render 'new'
     end
   end
-
   def edit
   end
+  def update
+    if params[:user][:password].empty? #Case3: handles - a failed update (which initially looks "successful") due to an empty password and confirmation
+      @user.errors.add(:password, "can't be empty")
+      render 'edit'
+    elsif @user.update_attributes(user_params) #Case4: handles - A successful update 
+      log_in @user
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit' #Case2: handles - A failed update due to an invalid password #No error/flash[:danger] ???
+    end
   
   private
-  
+    
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
     def get_user
       @user = User.find_by(email: params[:email])
     end
     def valid_user
       unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
         redirect_to root_url
+      end
+    end
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired."
+        redirect_to new_password_reset_url #path?
       end
     end
 end
