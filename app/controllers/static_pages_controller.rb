ca241f376f3a322030271215e817235803732113
Christian Meyer <cmeyerwps@gmail.com>
2018-03-27 19:04:17 -0400

    #home+feed and feed partial

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
   def home # GET / (Where '/' is the root)
-    @micropost = current_user.microposts.build if logged_in?
+    if logged_in?
+      @micropost = current_user.microposts.build 
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
   def help # GET /help
   end
